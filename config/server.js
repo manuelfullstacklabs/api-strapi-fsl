40921052ff4a286a284579673d53c87ab87e71b5
manueldev94 <raymer.rodriguez94@gmail.com>
2021-08-10 09:17:37 -0400

    Changes to my-project noted

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0116f379b48fc1843a853cdc18fced46'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a738f98e-f39c-44a2-8b36-11ea369a0cca'
+};
